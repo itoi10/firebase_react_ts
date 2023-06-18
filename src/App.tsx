9c0a185d14f9b500db61a5b7f573a6d9b705ff22
itict10 <itict10@gmail.com>
2023-06-18 15:44:23 +0900

    Add user state change

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,51 @@
-import './App.css';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
+import { auth } from "./firebase";
 
-function App() {
+
+const  App:React.FC = () => {
+  // userのstate取得
+  const user = useSelector(selectUser);
+  // アクションをdispatchするための関数を取得
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+      // onAuthStateChangedはログイン状態が変わると呼ばれる
+      const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+        if(userAuth) {
+          // ログインしたので、userのstateを更新
+          dispatch(login({
+            uid: userAuth.uid,
+            photoUrl: userAuth.photoURL,
+            displayName: userAuth.displayName,
+          }))
+        } else {
+          // ログアウト
+          dispatch(logout());
+        }
+      })
+
+      // コンポーネントがアンマウントされるときに実行される
+      return () => {
+        // onAuthStateChangedの監視を解除
+        unsubscribe();
+      }
+  }, [dispatch])
+
   return (
     <div className="App">
-
+      {/* テスト */}
+      {user !== null && user !== undefined ? (
+        <>
+        <p>ログインしました</p>
+        <p>{user.uid}</p>
+        <p>{user.displayName}</p>
+        <p>{user.photoUrl}</p>
+        </>
+      ) : (
+        <p>ログアウトしました</p>
+      )}
     </div>
   );
 }
