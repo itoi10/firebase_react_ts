6009ba65ebf1a5145ac20373256f1f8503325557
itict10 <itict10@gmail.com>
2023-06-24 14:46:40 +0900

    Add fetch posts

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,11 +1,60 @@
-import styles from './Feed.module.css'
-import PostInput from './PostInput'
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import React, { useEffect, useState } from 'react';
+import { db } from '../firebase';
+import styles from './Feed.module.css';
+import PostInput from './PostInput';
 
-export const Feed = () => {
+const Feed:React.FC = () => {
+
+  const [posts, setPosts] = useState([
+    {
+      id: "",
+      avatar:"",
+      image:"",
+      text:"",
+      timestamp:null,
+      username:"",
+    }
+  ]);
+
+    // Fetch Posts
+    useEffect(() => {
+      const q = query(
+        collection(db, "posts"),
+        orderBy("timestamp", "desc"),
+      );
+      const unsubscribe = onSnapshot(q, (snapshot) => {
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            avatar: doc.data().avatar,
+            image: doc.data().image,
+            text: doc.data().text,
+            timestamp: doc.data().timestamp,
+            username: doc.data().username,
+          }))
+        )
+      });
+
+      console.log(posts)
+
+      return () => {
+        unsubscribe();
+      };
+    }, []);
+
   return (
     <div className={styles.feed}>
-      Feed
+
       <PostInput />
+      {posts.map(post => (
+        <h3>{post.id}</h3>
+      ))}
+
     </div>
   )
 }
+
+export default Feed;
+
+
